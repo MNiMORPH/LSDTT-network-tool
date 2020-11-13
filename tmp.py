f7040fd3e9a5aa9ee7dfc60fc057d5a782f65fc7
Peter Mitchell <pmitchell490@icloud.com>
2020-11-13 11:24:55 -0600

    Updated to work with new chi outputs.

--- tmp.py
+++ tmp.py
@@ -9,8 +9,8 @@ from shapely.geometry import LineString
 
 window = 1000 # meters per "reach"
 
-rp = pd.read_csv('/Users/Shanti/Desktop/Fall_2020/LSDTT-network/LSDTT-network/ww_everything_newDTB.csv', index_col='node')
-#rp = pd.read_csv('C:/LSDTopoTools/data/UMN/LSDTT-network/ww_everything_newDTB.csv', index_col='node')
+#rp = pd.read_csv('/Users/Shanti/Desktop/Fall_2020/LSDTT-network/LSDTT-network/ww_everything_newDTB.csv', index_col='node')
+rp = pd.read_csv('WW_LSDTTNetwork_chi_outputs_wholewatershed_chi_data_map.csv', index_col='NI')
 #rp = pd.read_csv('/home/andy/Desktop/Eel_River_Network_testing/Eel_River_DEM_MChiSegmented.csv')
 segment_ids = np.array(list(set(rp['source_key'])))
 
@@ -22,16 +22,16 @@ rp.insert(len(rp.columns), 'receiver_source_key', None)
 #for _node in rp.index:
 # IMPORTANT FOR EFFICIENCY: MINIMIZE THE NUMBER OF TIMES YOU UPDATE THE
 # DATAFRAME
+
 _tmplist = []
-for _receiver_node in rp['receiver_node']:
-    #_receiver_node = rp.loc[_node, 'receiver_node']
-    try:
-        _receiver_source_key = rp.loc[_receiver_node, 'source_key']
-    except:
+for _node in rp.index:
+    if _node != rp.loc[_node, 'receiver_NI']:
+        _receiver_source_key = rp.loc[rp.loc[_node, 'receiver_NI'], 'source_key']
+    else:
         print("Found mouth node. Offmap receiver node ID: "
-                + str(_receiver_node))
+                + str(_node))
         _receiver_source_key = -1
-        receiver_nodes_at_mouths.append(_receiver_node)
+        receiver_nodes_at_mouths.append(_node)
     _tmplist.append(_receiver_source_key)
 rp['receiver_source_key'] = _tmplist
 
@@ -40,7 +40,7 @@ mouth_nodes = list(rp[rp['receiver_source_key'] == -1].index)
 
 # Next, identify these confluences by places where the receiver_source_key
 # differs from the source_key
-confluence_downstream_nodes = list(set(list(rp['receiver_node']
+confluence_downstream_nodes = list(set(list(rp['receiver_NI']
                                                   [rp['source_key'] !=
                                                   rp['receiver_source_key']])))
 # Remove river mouths
@@ -54,16 +54,16 @@ for _receiver_node_at_mouth in receiver_nodes_at_mouths:
 # Create a set of confluence locations
 confluences = []
 for _node in confluence_downstream_nodes:
-    _x = rp.loc[_node, 'E_UTM']
-    _y = rp.loc[_node, 'N_UTM']
+    _x = rp.loc[_node, 'longitude']
+    _y = rp.loc[_node, 'latitude']
     confluences.append([_x, _y])
 confluences = np.array(confluences)
 
 # Create a set of river mouth locations
 mouths = []
 for _node in mouth_nodes:
-    _x = rp.loc[_node, 'E_UTM']
-    _y = rp.loc[_node, 'N_UTM']
+    _x = rp.loc[_node, 'longitude']
+    _y = rp.loc[_node, 'latitude']
     mouths.append([_x, _y])
 mouths = np.array(mouths)
 
@@ -91,9 +91,9 @@ termination_nodes = np.hstack(( confluence_downstream_nodes, mouth_nodes ))
 segments_nodes = []
 for _source_node in source_nodes:
     segment_nodes = [_source_node]
-    segment_nodes.append(rp.loc[segment_nodes[-1], 'receiver_node'])
+    segment_nodes.append(rp.loc[segment_nodes[-1], 'receiver_NI'])
     while segment_nodes[-1] not in termination_nodes:
-        segment_nodes.append(rp.loc[segment_nodes[-1], 'receiver_node'])
+        segment_nodes.append(rp.loc[segment_nodes[-1], 'receiver_NI'])
     segments_nodes.append(segment_nodes)
 
 # Next, reconstruct the data table elements for each of these points
@@ -162,19 +162,19 @@ dfsegs.insert(len(dfsegs.columns), 'min_elev', None)
 dfsegs.insert(len(dfsegs.columns), 'average_elev', None)
 dfsegs.insert(len(dfsegs.columns), 'drainage_area_km2', None)
 dfsegs.insert(len(dfsegs.columns), 'chi', None)
-dfsegs.insert(len(dfsegs.columns), 'depth_to_bedrock_m', None)
+#dfsegs.insert(len(dfsegs.columns), 'depth_to_bedrock_m', None)
 for i in range(len(segments)):
     segment = segments[i]
-    dfsegs['slope'][i] = (np.max(segment['z']) - np.min(segment['z'])) / \
+    dfsegs['slope'][i] = (np.max(segment['elevation']) - np.min(segment['elevation'])) / \
                          ( np.max(segment['flow_distance']) \
                            - np.min(segment['flow_distance']) )
-    dfsegs['average_elev'][i] = (np.max(segment['z']) + np.min(segment['z'])) / 2
-    dfsegs['max_elev'][i] = (np.max(segment['z']))
-    dfsegs['min_elev'][i] = (np.min(segment['z']))
+    dfsegs['average_elev'][i] = (np.max(segment['elevation']) + np.min(segment['elevation'])) / 2
+    dfsegs['max_elev'][i] = (np.max(segment['elevation']))
+    dfsegs['min_elev'][i] = (np.min(segment['elevation']))
     dfsegs['drainage_area_km2'][i] = np.mean(segment['drainage_area'])/1E6
     dfsegs['chi'][i] = np.mean(segment['chi'])
     # These are going to be particular to this case
-    dfsegs['depth_to_bedrock_m'][i] = np.mean(segment['depth_to_bedrock'])
+    #dfsegs['depth_to_bedrock_m'][i] = np.mean(segment['depth_to_bedrock'])
     #dfsegs['bedrock_lithology'] = np.mean(segment['depth_to_bedrock'])
 
 
@@ -183,7 +183,7 @@ for i in range(len(segments)):
 stream_lines = []
 for segment in segments:
     stream_lines.append( LineString(
-                            segment.loc[:, ('lon', 'lat', 'z')].values ) )
+                            segment.loc[:, ('longitude', 'latitude', 'elevation')].values ) )
 
 # Now convert to geopandas
 gdf_segs = gpd.GeoDataFrame( dfsegs, geometry=stream_lines )
@@ -192,85 +192,6 @@ gdf_segs = gpd.GeoDataFrame( dfsegs, geometry=stream_lines )
 gdf_segs.to_file('whitewater_river_segments.gpkg', driver="GPKG")
 
 print("Done!")
-
-"""
-# Create the lines for the shapefile
-sf_lines = []
-for segment in segments:
-    _tmparr = segment.loc[:, ('lon', 'lat', 'z')].values
-    _tmplist = []
-    for row in _tmparr:
-        _tmplist.append(list(row))
-    sf_lines.append( _tmplist )
-
-# Now write the results to a shapefile
-sf = shapefile.Writer('ww-lines')
-sf.shapeType = shapefile.POLYLINEZ # 13; POLYLINE=3
-sf.field('id', 'N')
-sf.field('toseg', 'N')
-sf.field('slope', 'N', decimal=7)
-sf.field('drainage_area_km2', 'N', decimal=6)
-sf.field('chi', 'N', decimal=3)
-# Specific to this data set
-sf.field('depth_to_bedrock_m', 'N', decimal=1)
-for i in range(len(dfsegs)):
-    sf.record( id = dfsegs['id'][i],
-              toseg = dfsegs['toseg'][i],
-              slope = dfsegs['slope'][i],
-              drainage_area_km2 = dfsegs['drainage_area_km2'][i],
-              chi = dfsegs['chi'][i],
-              depth_to_bedrock_m = dfsegs['depth_to_bedrock_m'][i] )
-sf.linez(sf_lines[i])
-sf.close()
-"""
-
-
-'''
-# Export the points (mouths, heads, confluences)
-
-# Create DataFrames of only these nodes
-_df_mouths = rp.loc[mouth_nodes, :]
-_df_heads = rp.loc[channel_head_nodes, :]
-_df_confluences = rp.loc[confluence_downstream_nodes, :]
-# But these don't have the extra info in them. So:
-# START HERE
-_pdout = []
-# for
-
-# Add in a "type" parameter
-_df_mouths['network_node_type'] = 'mouth'
-_df_heads['network_node_type'] = 'head'
-_df_confluences['network_node_type'] = 'confluence'
-
-# All nodes in network
-_df_NetworkNodes = pd.concat([_df_heads, _df_confluences, _df_mouths])
-
-gdf_NetworkNodes = geopandas.GeoDataFrame( _df_NetworkNodes,
-                                           geometry=geopandas.points_from_xy(
-                                                _df_NetworkNodes.lon,
-                                                _df_NetworkNodes.lat) )
-
-# Save to shapefile -- some field names will be truncated
-gdf_NetworkNodes.to_file('whitewater_river_nodes')
-
-
-
-
-# Export for Landlab NetworkSedimentTransporter
-
-# Create a geoDataFrame with the minimum required data
-NST_nodes = gdf_NetworkNodes.loc[:, [ 'receiver_node',
-                                      'lon', 'lat', 'z',
-                                      'drainage_area', 'geometry'] ]
-
-# Add NST_nodes.index as the node ID
-
-# Build the
-
-'''
-
-
-
 
 #Generating the geopackage to begin path selection
 print('Now I will create a geopackage to select segments for a path.')
@@ -282,7 +203,7 @@ dfsegsselect= pd.DataFrame ({'segment_ID':segment_ids, 'toseg':toseg})
 stream_lines_select = []
 for segment in segments:
     stream_lines_select.append( LineString(
-                            segment.loc[:, ('lon', 'lat', 'z')].values ) )
+                            segment.loc[:, ('longitude', 'latitude', 'elevation')].values ) )
 
 gdf_segsselect = gpd.GeoDataFrame( dfsegsselect, geometry=stream_lines_select )
 
@@ -361,25 +282,25 @@ dfpath_nodes
 # Profile of entire network (selected path in red)
 plt.figure()
 for segment in segments:
-    plt.plot(segment['flow_distance'], segment['z'], 'k-')
-plt.plot(dfpath_nodes['flow_distance'], dfpath_nodes['z'], 'r-')
+    plt.plot(segment['flow_distance'], segment['elevation'], 'k-')
+plt.plot(dfpath_nodes['flow_distance'], dfpath_nodes['elevation'], 'r-')
 
 # Map view of network (selected path in red)
 plt.figure()
 for segment in segments:
-    plt.plot(segment['E_UTM'], segment['N_UTM'])
+    plt.plot(segment['longitude'], segment['latitude'])
 
 plt.plot(confluences[:,0], confluences[:,1], 'bo')
 plt.plot(mouths[:,0], mouths[:,1], 'ro')
-plt.plot(dfpath_nodes['E_UTM'], dfpath_nodes['N_UTM'], 'r-')
+plt.plot(dfpath_nodes['longitude'], dfpath_nodes['latitude'], 'r-')
 
 plt.figure()
 
 # Map view of the selected path
-plt.plot(dfpath_nodes['E_UTM'], dfpath_nodes['N_UTM'], 'k-')
+plt.plot(dfpath_nodes['longitude'], dfpath_nodes['latitude'], 'k-')
 plt.figure()
 
 # Long profile of the selected path
-plt.plot(dfpath_nodes['flow_distance'], dfpath_nodes['z'])
+plt.plot(dfpath_nodes['flow_distance'], dfpath_nodes['elevation'])
 
 plt.show()
